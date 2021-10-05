573ab41afbf7290f7e125f09fea09391d37a552b
Dmitry Rybkin <31039440+DmitryRybkin@users.noreply.github.com>
2021-10-05 16:12:52 +0300

    Update CheckUploadProgress.php

--- CheckUploadProgress.php
+++ CheckUploadProgress.php
@@ -1,5 +1,13 @@
 <?php
 
+$vimeoClient = new Vimeo($clientId, $clientSecret, $accessToken);
+$data = [
+    'name' => 'Vimeo API SDK test upload',
+    'description' => "This video was uploaded through the Vimeo API's PHP SDK."
+];
+$uri = '/me/videos?fields=uri,upload';
+$attempt = $vimeoClient->request($uri, $data, 'POST', true, []);
+
 $uploadLink = 'https://europe-files.tus.vimeo.com/files/vimeo-prod-src-tus-eu/10cdd25b42393b55bf933af502c14304';
 $curl_opts = array(
     CURLOPT_CUSTOMREQUEST => 'HEAD',
@@ -8,6 +16,7 @@ $curl_opts = array(
         'Tus-Resumable: 1.0.0',
     )
 );
+
 $curl = curl_init($uploadLink);
 curl_setopt_array($curl, $curl_opts);
 $response = curl_exec($curl);
